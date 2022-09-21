3014316d87b33e491504864490f3f5b21b657142
Ivan <ivan.zhivodovsky32@mail.ru>
2022-09-21 12:09:16 +0300

    Add connection to DB

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+
+using MySql.Data.MySqlClient;
 
 namespace ESOperatorTaxi
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            string connStr = "server=s2.kts.tu-bryansk.ru;port=3306;user=IAS18.ZHivII;database=IAS18_ZHivII;password=3q%Md=Q2/4;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string sql = "SELECT * FROM clients";
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGrid.AutoGenerateColumns = true;
+            dataGrid.ItemsSource = dt.DefaultView;
+            conn.Close();
         }
+
+
     }
 }
