52c1fee47b3a7d821343a985f6c9bdbf1f60805c
Ivan <64004982+ivanzhiv32@users.noreply.github.com>
2023-02-08 22:10:24 +0300

    Add class TripTicket

---
+++ TripTicket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESOperatorTaxi
+{
+    class TripTicket:Entity
+    {
+        private int milleage;
+        private DateTime dateStart;
+        private DateTime dateEnd;
+
+        public int Milleage { get => milleage; set => Set(ref milleage, value); }
+        public DateTime DateStart { get => dateStart; set => Set(ref dateStart, value); }
+        public DateTime DateEnd { get => dateEnd; set => Set(ref dateEnd, value); }
+    }
+}
