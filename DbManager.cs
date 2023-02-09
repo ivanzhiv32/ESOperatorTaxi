b90adc788994933300368d7ea72b5bc0c9c31d68
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2023-02-09 20:36:25 +0300

    Add method Add in class DbManager

--- DbManager.cs
+++ DbManager.cs
@@ -8,6 +8,7 @@ using MySql.Data.MySqlClient;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
+using System.Collections;
 
 namespace ESOperatorTaxi
 {
@@ -29,7 +30,7 @@ namespace ESOperatorTaxi
             //LoadOrders();
         }
 
-        public ObservableCollection<Client> Clients { get; private set; }
+        public ObservableCollection<Client> Clients { get; private set; } = new ObservableCollection<Client>();
         public ObservableCollection<Car> Cars { get; private set; }
         public ObservableCollection<Driver> Drivers { get; private set; }
         public ObservableCollection<Order> Orders { get; private set; }
@@ -39,17 +40,36 @@ namespace ESOperatorTaxi
         public Dictionary<int, string> ClassesOrders = new Dictionary<int, string>();
 
         /// <summary>
+        /// Найти коллекцию сущностей по типу сущности
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        // NOTE: попробовать реализовать поиск через рефлексию
+        public IList FindCollectionByType<T>() where T: Entity 
+        {
+            Type type = typeof(T);
+            switch (type.Name)
+            {
+                case nameof(Client):
+                    return Clients;
+                // Добавить остальные коллекции
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
         /// Загрузить сущности из таблицы базы данных по типу
         /// </summary>
         /// <typeparam name="T">Тип сущности</typeparam>
         /// <returns>Список сущностей</returns>
         public ICollection<T> LoadEntities<T>() where T : Entity, new()
         {
-            Type typeItem = typeof(T);
-            var result = new List<T>();
-            if (typeItem.GetCustomAttributes(typeof(TableAttribute), true).FirstOrDefault() is TableAttribute tableAttr)
+            try
             {
-                try
+                Type typeEntity = typeof(T);
+                var result = new List<T>();
+                if (typeEntity.GetCustomAttributes(typeof(TableAttribute), true).FirstOrDefault() is TableAttribute tableAttr)
                 {
                     dbConnection.Open();
 
@@ -62,7 +82,7 @@ namespace ESOperatorTaxi
                     }
 
                     var columnAttrs = new Dictionary<PropertyInfo, ColumnAttribute>();
-                    foreach (var prop in typeItem.GetProperties())
+                    foreach (var prop in typeEntity.GetProperties())
                     {
                         var attr = prop.GetCustomAttributes(typeof(ColumnAttribute), true).FirstOrDefault() as ColumnAttribute;
                         if (attr != null)
@@ -80,23 +100,78 @@ namespace ESOperatorTaxi
                         result.Add(item);
                     }
                 }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-                finally
-                {
-                    if (dbConnection.State != ConnectionState.Closed)
-                        dbConnection.Close();
-                }
+                return result;
             }
-
-            return result;
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dbConnection.State != ConnectionState.Closed)
+                    dbConnection.Close();
+            }
         }
 
         public void Load() { }
 
-        public void Add<T>(T entity) where T : Entity { }
+        public void Add<T>(T entity) where T : Entity 
+        {
+            try
+            {
+                Type typeEntity = typeof(T);
+                if (typeEntity.GetCustomAttributes(typeof(TableAttribute), true).FirstOrDefault() is TableAttribute tableAttr)
+                {
+                    // Подготовка аргументов для Sql-команды
+                    var arguments = new Dictionary<string, object>();
+                    //var columnAttrs = new Dictionary<PropertyInfo, ColumnAttribute>();
+                    foreach (var prop in typeEntity.GetProperties())
+                    {
+                        var attr = prop.GetCustomAttributes(typeof(ColumnAttribute), true).FirstOrDefault() as ColumnAttribute;
+                        if (attr != null && attr.Name != "ID")
+                            arguments.Add(attr.Name, prop.GetValue(entity));
+                        //columnAttrs.Add(prop, attr);
+                    }
+                    
+                    // Создание Sql-комаынды
+                    StringBuilder sqlColumns = new StringBuilder($"INSERT {tableAttr.Name} (");
+                    StringBuilder sqlValues = new StringBuilder("VALUES (");
+                    foreach (var arg in arguments)
+                    {
+                        sqlColumns.Append($"{arg.Key},");
+                        //sqlValues.Append($"{arg.Value},");
+                        sqlValues.Append($"@{arg.Key},");
+                    }
+                    sqlColumns.Remove(sqlColumns.Length - 1, 1);
+                    sqlColumns.Append(")");
+                    sqlValues.Remove(sqlValues.Length - 1, 1);
+                    sqlValues.Append(")");
+                    string sql = sqlColumns.Append(" " + sqlValues.ToString() + "; SELECT LAST_INSERT_ID();").ToString();
+
+                    // Создание и выполнение Sql-команды
+                    dbConnection.Open();
+                    MySqlCommand sqlCommand = new MySqlCommand(sql.ToString(), dbConnection);
+                    foreach (var arg in arguments) // Добавление параметров
+                        sqlCommand.Parameters.AddWithValue($"@{arg.Key}", arg.Value);
+                    var result = sqlCommand.ExecuteScalar();
+                    int.TryParse(result.ToString(), out int id);
+                    entity.Id = id;
+
+                    // Добавление сущности в локальное хранилище
+                    var collection = FindCollectionByType<T>();
+                    collection?.Add(entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dbConnection.State != ConnectionState.Closed)
+                    dbConnection.Close();
+            }
+        }
 
         public void Update<T>(T entity) where T : Entity { }
 
@@ -128,36 +203,36 @@ namespace ESOperatorTaxi
             }
         }
 
-        public void LoadCars()
-        {
-            Cars = new ObservableCollection<Car>();
+        //public void LoadCars()
+        //{
+        //    Cars = new ObservableCollection<Car>();
 
-            string sql = "SELECT * FROM cars";
-            MySqlCommand sqlCommand = new MySqlCommand(sql, dbConnection);
-            DataTable dt = new DataTable();
-            using (MySqlDataReader reader = sqlCommand.ExecuteReader())
-                dt.Load(reader);
+        //    string sql = "SELECT * FROM cars";
+        //    MySqlCommand sqlCommand = new MySqlCommand(sql, dbConnection);
+        //    DataTable dt = new DataTable();
+        //    using (MySqlDataReader reader = sqlCommand.ExecuteReader())
+        //        dt.Load(reader);
 
 
-            foreach (DataRow row in dt.Rows)
-            {
-                string sqlClass = "SELECT Name FROM car_classes WHERE ID=" + row["IdClass"];
-                string carClass = null;
-                sqlCommand = new MySqlCommand(sqlClass, dbConnection);
-                using (MySqlDataReader reader = sqlCommand.ExecuteReader())
-                    if (reader.Read()) carClass = (string)reader.GetValue(0);
+        //    foreach (DataRow row in dt.Rows)
+        //    {
+        //        string sqlClass = "SELECT Name FROM car_classes WHERE ID=" + row["IdClass"];
+        //        string carClass = null;
+        //        sqlCommand = new MySqlCommand(sqlClass, dbConnection);
+        //        using (MySqlDataReader reader = sqlCommand.ExecuteReader())
+        //            if (reader.Read()) carClass = (string)reader.GetValue(0);
 
-                Cars.Add(
-                    new Car()
-                    {
-                        Id = (int)row["ID"],
-                        Brand = (string)row["Brand"],
-                        Number = (string)row["Number"],
-                        Colour = (string)row["Colour"],
-                        ClassName = carClass,
-                    });
-            }
-        }
+        //        Cars.Add(
+        //            new Car()
+        //            {
+        //                Id = (int)row["ID"],
+        //                Brand = (string)row["Brand"],
+        //                Number = (string)row["Number"],
+        //                Colour = (string)row["Colour"],
+        //                ClassName = carClass,
+        //            });
+        //    }
+        //}
 
         public void LoadDrivers()
         {
