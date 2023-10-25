f095c4fed08689bbf9a590c2b6917c34892acede
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2023-10-25 22:23:44 +0300

    Create operator service

---
+++ OperatorService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ESOperatorTaxi
+{
+    class OperatorService
+    {
+        private IEnumerable<PriceRule> priceRules;
+        private IEnumerable<Driver> drivers;
+
+        public OperatorService(IEnumerable<PriceRule> priceRules, IEnumerable<Driver> drivers) 
+        {
+            this.priceRules = priceRules;
+            this.drivers = drivers;
+        }
+
+        /// <summary>
+        /// Рассчитать базовую стоимость поездки
+        /// </summary>
+        /// <param name="geoPointFrom"></param>
+        /// <param name="geoPointTo"></param>
+        /// <param name="orderClass"></param>
+        /// <returns></returns>
+        public decimal CalculateBasePrice(Geopoint geoPointFrom, Geopoint geoPointTo, OrderClass orderClass) 
+        {
+            if (geoPointFrom is null)
+                throw new ArgumentNullException(nameof(geoPointFrom));
+            if (geoPointTo is null)
+                throw new ArgumentNullException(nameof(geoPointTo));
+
+            double distance = geoPointFrom.GetDistanceTo(geoPointTo);
+            decimal price = 0; // Какую базовую ставку по-умолчанию указать и нужно ли ее указывать?
+            bool isRuleFound = false;
+            foreach (PriceRule rule in priceRules)
+            {
+                if (distance > rule.StartRange && distance < rule.EndRange && rule.OrderClass == orderClass) 
+                {
+                    price = rule.PricePerKm * (decimal)distance;
+                    isRuleFound = true;
+                    break;
+                }
+            }
+
+            if (!isRuleFound)
+                throw new Exception("Не найдено правило для определения базовой ставки.");
+
+            return price;
+        }
+
+        /// <summary>
+        /// Подобрать водителей для заявки
+        /// </summary>
+        /// <param name="geoPointFrom"></param>
+        /// <param name="orderClass"></param>
+        /// <returns>Выбранные водители в формате: водитель - степень соответствия</returns>
+        public Dictionary<Driver, double> ChooseDrivers(Geopoint geoPointFrom, OrderClass orderClass) 
+        {
+            if (geoPointFrom is null)
+                throw new ArgumentNullException(nameof(geoPointFrom));
+
+            return null;
+        }
+    }
+}
---
+++ PriceRule.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ESOperatorTaxi
+{
+    /// <summary>
+    /// Правило для определения базовой ставки стоимости проезда за Км
+    /// </summary>
+    class PriceRule : Entity
+    {
+        private int startRange;
+        private int endRange;
+        private OrderClass orderClass;
+        private decimal pricePerKm;
+
+        public int StartRange { get => startRange; set => Set(ref startRange, value); }
+        public int EndRange { get => endRange; set => Set(ref endRange, value); }
+        public OrderClass OrderClass { get => orderClass; set => Set(ref orderClass, value); }
+        public decimal PricePerKm { get => pricePerKm; set => Set(ref pricePerKm, value); }
+    }
+}
